b4e6828cbc690ff7e1c106c657203f691433dfa6
Cmarco2002 <carlosmarcogonzalez@gmail.com>
2022-03-03 18:06:26 +0100

    Segundo commit

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,5 +1,35 @@
 package examen2programacion;
 
-public class Ejercicio3 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Ejercicio3 {
+	public static ArrayList<String> informacionVideojuegos() {
+		ArrayList<String> consolas = new ArrayList<String>();
+		String nombre = "C:\\Users\\Carlos\\Desktop\\ventasVideojuegos.csv";
+		File file = new File(nombre);
+		try {
+			Scanner f = new Scanner(file);
+			// Vamos linea por linea
+			while (f.hasNextLine()) {
+				String linea = f.nextLine();
+				// Spliteamos los datos separados por comas
+				String[] lineaSeparada = linea.split(",");
+				if (consolas.contains(lineaSeparada[2])||lineaSeparada[2].length()>4) {
+				} else {
+					consolas.add(lineaSeparada[2]);
+				}
+			}
+			f.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("El fichero " + nombre + " no ha podido ser abierto.");
+		}
+		return consolas;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(informacionVideojuegos());
+	}
 }
